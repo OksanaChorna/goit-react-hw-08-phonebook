ee1ba94078168fc589d190d4623ac019ad8bff21
Oksana Chorna <ksuchorna@gmail.com>
2021-06-14 17:41:25 +0300

    Приватний маршрут

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage';
 import ContactsPage from './pages/ContactsPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import PrivateRoute from './components/PrivateRoute';
 
 class App extends Component {
   componentDidMount() {
@@ -21,7 +22,11 @@ class App extends Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/login" component={LoginPage} />
-          <Route path="/contacts" component={ContactsPage} />
+          <PrivateRoute
+            path="/contacts"
+            redirectTo="/login"
+            component={ContactsPage}
+          />
           <Redirect to="/" />
         </Switch>
       </>
